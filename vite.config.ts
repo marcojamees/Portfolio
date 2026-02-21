32ba61c6aa21e74b56d8cd9f31b7c5e44ae4c6c5
marcojamees <james.marco118@gmail.com>
2026-02-21 01:21:08 -0300

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 // IMPORTANTE: Altere "Projeto" para o nome do seu repositório no GitHub
 const repoName = "Projeto";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: `/${repoName}/`,
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
